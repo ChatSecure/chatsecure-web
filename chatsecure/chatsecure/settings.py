c460b290732cd3db6d95e875f700da267e231cce
Chris Ballinger <chrisballinger@gmail.com>
2013-12-12 12:21:44 -0800

    settings.py changes

--- chatsecure/chatsecure/settings.py
+++ chatsecure/chatsecure/settings.py
@@ -33,7 +33,8 @@ LANGUAGE_CODE = 'en-us'
 
 SITE_ID = 1
 
-ALLOWED_HOSTS = ["chatsecure.org", "www.chatsecure.org"]
+# Moved to local_settings.py
+#ALLOWED_HOSTS = ["chatsecure.org", "www.chatsecure.org"]
 
 # If you set this to False, Django will make some optimizations so as not
 # to load the internationalization machinery.
@@ -53,7 +54,7 @@ MEDIA_ROOT = os.path.join(os.path.dirname(__file__), '../../../media')
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = ''
+MEDIA_URL = '/media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
